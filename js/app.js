201bfe49477826c30740ca8ddc927844d6319ecc
Artem <merkushynletter13@gmail.com>
2022-03-31 21:12:01 +0300

    created audio

--- js/app.js
+++ js/app.js
@@ -23,4 +23,22 @@ window.onscroll = function() {
    })
 }
 
-window.scrollTo(0, 1)
+window.scrollTo(0, 1);
+
+//audio
+
+let soundButton = document.querySelector('.soundbutton');
+let audio = document.querySelector('.audio');
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused');
+	audio.paused ? audio.play() : audio.pause();
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play();
+}
+
+window.onblur = function() {
+	audio.pause();
+}
